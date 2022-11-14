d93e764b2172259d5260954065dd6f12645f3e36
youssefksouri <49146351+youssefksouri@users.noreply.github.com>
2022-11-14 13:26:22 +0100

    Update Facture.java

--- src/main/java/tn/esprit/rh/achat/entities/Facture.java
+++ src/main/java/tn/esprit/rh/achat/entities/Facture.java
@@ -14,14 +14,14 @@ import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+//import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 public class Facture implements Serializable {
 	private static final long serialVersionUID = 1L;
 
